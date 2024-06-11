bd4d5c56c6a6aa83ccfc42634b1a076ca56e91e5
Parth Kapadia <parthkapadia65@gmail.com>
2024-06-10 20:13:43 -0400

    add view and models

---
+++ models/Blog.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Blog extends Model { }
+// IT checks  the password against the encrypted password in the database.
+
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog',
+    }
+);
+
+module.exports = Blog;
---
+++ models/Comment.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Comment extends Model { }
+// It checks the password against the encrypted password in the database.
+
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+module.exports = Comment;
